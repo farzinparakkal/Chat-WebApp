f8a689c4b8ef3499dc67e931d83ad909e73cfedb
farzinparakkal <142653177+farzinparakkal@users.noreply.github.com>
2025-03-02 15:07:52 +0530

    Update Nav.jsx

--- clientside/src/components/Nav.jsx
+++ clientside/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, , useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../scss/Nav.scss";
 
 const Nav = ({ profile, setName }) => {
